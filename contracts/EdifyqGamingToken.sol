eaf9b5121ed38ac549164d74461c1489a961bf47
edifyq-admin <89722099+edifyq-admin@users.noreply.github.com>
2024-06-28 13:50:05 +0100

    update bulk distribution in gaming token contract (#1)

--- contracts/EdifyqGamingToken.sol
+++ contracts/EdifyqGamingToken.sol
@@ -2,8 +2,16 @@
 pragma solidity ^0.8.19;
 
 contract EdifyqGamingToken {
+
+    struct Recipient {
+        address recipient;
+        uint256 value;
+    }
+
     mapping(address account => uint256) private _balances;
     mapping(address account => mapping(address spender => uint256)) private _allowances;
+
+    address private _owner;
 
     uint256 private _totalSupply = 1000000;
 
@@ -15,6 +23,7 @@ contract EdifyqGamingToken {
 
     constructor() {
         _balances[msg.sender] = 1000000;
+        _owner = msg.sender;
     }
 
     function allowance(address owner, address spender) public view returns(uint256) {
@@ -29,6 +38,19 @@ contract EdifyqGamingToken {
 
     function balanceOf(address account) public view returns(uint256) {
         return _balances[account];
+    }
+
+    function bulk(Recipient[] memory recipients, uint256 total) external returns(bool) {
+        require(msg.sender == _owner, "Must be the contract owner");
+
+        uint256 length = recipients.length;
+
+        for (uint256 i; i < length; i++) {
+            _balances[recipients[i].recipient] += recipients[i].value;
+        }
+        _balances[_owner] -= total;
+
+        return true;
     }
 
     function decimals() external pure returns (uint8) {
@@ -58,8 +80,8 @@ contract EdifyqGamingToken {
     }
 
     function transferFrom(address from, address to, uint256 value) public returns(bool) {
-        // address spender = msg.sender;
-        // _spendAllowance(from, spender, value);
+        address spender = msg.sender;
+        _spendAllowance(from, spender, value);
         _transfer(from, to, value);
         return true;
     }
@@ -80,6 +102,16 @@ contract EdifyqGamingToken {
     function _mint(address account, uint256 value) internal {
         require(account != address(0), "Invalid receiver");
         _update(address(0), account, value);
+    }
+
+    function _spendAllowance(address owner, address spender, uint256 value) internal virtual {
+        uint256 currentAllowance = allowance(owner, spender);
+        if (currentAllowance != type(uint256).max) {
+            require(currentAllowance >= value, "Insufficient allowance");
+            unchecked {
+                _approve(owner, spender, currentAllowance - value);
+            }
+        }
     }
 
     function _transfer(address from, address to, uint256 value) private {
